9e6701f73417697656d869effc080ef733f2478b
ghengeli <greg.hengeli+github@gmail.com>
2011-09-10 15:02:39 -0700

    expose genability tariff

--- service/src/main/java/com/tendril/cleanweb/server/DispatchServletModule.java
+++ service/src/main/java/com/tendril/cleanweb/server/DispatchServletModule.java
@@ -3,6 +3,7 @@ package com.tendril.cleanweb.server;
 import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import com.tendril.cleanweb.server.resource.TariffResource;
 import com.tendril.cleanweb.server.resource.UserResource;
 
 import java.util.HashMap;
@@ -14,6 +15,7 @@ public class DispatchServletModule extends ServletModule {
 
     @Override
     public void configureServlets() {
+        bind(TariffResource.class);
         bind(UserResource.class);
 
         Map<String, String> params = new HashMap<String, String>();
---
+++ service/src/main/java/com/tendril/cleanweb/server/resource/TariffResource.java
@@ -0,0 +1,25 @@
+package com.tendril.cleanweb.server.resource;
+
+import com.tendril.cleanweb.GenabilityClient;
+import com.tendril.cleanweb.domain.Tariff;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.List;
+
+/**
+ */
+@Path("/tariff")
+public class TariffResource {
+
+    @Inject
+    GenabilityClient genabilityClient;
+
+    @GET
+    public List<Tariff> getTariffs(@QueryParam("zipcode") String zipCode) {
+        return genabilityClient.getTariffs(zipCode);
+    }
+
+}
