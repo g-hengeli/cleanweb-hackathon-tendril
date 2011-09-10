1786500a127cec0bd57d35e9d08fd0cb69cec2b3
ghengeli <greg.hengeli+github@gmail.com>
2011-09-10 14:50:44 -0700

    dummy user resource

--- service/src/main/java/com/tendril/cleanweb/server/resource/UserResource.java
+++ service/src/main/java/com/tendril/cleanweb/server/resource/UserResource.java
@@ -4,14 +4,22 @@ import com.tendril.cleanweb.domain.User;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 /**
  */
-@Path("user")
+@Path("/user")
+@Produces("application/json")
 public class UserResource {
 
     @GET
     public User getUser() {
         return new User("fname", "lname", "80023");
+    }
+
+    @GET
+    @Path("/test")
+    public String getUserTest() {
+        return "test";
     }
 }
