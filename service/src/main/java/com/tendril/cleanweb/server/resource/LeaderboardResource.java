f5f5f070586cbfc51376ad3989f718183d6715ad
Jacob Kaufman-Osborn <jacobko@gmail.com>
2011-09-11 13:40:18 -0700

    Added neighbors method to leaderboard resource.

--- service/src/main/java/com/tendril/cleanweb/server/resource/LeaderboardResource.java
+++ service/src/main/java/com/tendril/cleanweb/server/resource/LeaderboardResource.java
@@ -1,7 +1,6 @@
 package com.tendril.cleanweb.server.resource;
 
-import com.tendril.cleanweb.domain.LeaderboardEntry;
-import com.tendril.cleanweb.service.LeaderboardService;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -10,26 +9,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Logger;
+
+import com.tendril.cleanweb.domain.LeaderboardEntry;
+import com.tendril.cleanweb.service.LeaderboardService;
 
 @Path("/leaderboard")
 @Produces(MediaType.APPLICATION_JSON)
 public class LeaderboardResource {
-	static Logger log = Logger.getLogger(LeaderboardResource.class.getName());
 
 	@Inject
 	private LeaderboardService leaderboardService;
-
-	// @POST
-	// @Consumes(MediaType.APPLICATION_JSON)
-	// public void createLeaderboardEntry(LeaderboardEntry entry) throws
-	// JSONException {
-	// log.info(entry.getZipcode() + " " + entry.getTariffName() + " " +
-	// entry.getScore());
-	// // log.info(entry);
-	// }
 
 	@POST
 	public void createLeaderboardEntry(@QueryParam("userId") String userId,
@@ -39,20 +28,27 @@ public class LeaderboardResource {
 		LeaderboardEntry entry = new LeaderboardEntry(userId, zipcode, score,
 				tariffName);
 
-        leaderboardService.persist(entry);
-
-		log.info(entry.toString());
+		leaderboardService.persist(entry);
 	}
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<LeaderboardEntry> getLeaderboard() {
-        return leaderboardService.getLeaderboard();
+		return leaderboardService.getLeaderboard();
+	}
 
-//		return tempLeaderboard;
-    }
+	@POST
+	@Path("/neighbors")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<LeaderboardEntry> getNeighbors(
+			@QueryParam("userId") String userId,
+			@QueryParam("zipcode") String zipcode,
+			@QueryParam("tariffName") String tariffName,
+			@QueryParam("score") int score) {
+		LeaderboardEntry entry = new LeaderboardEntry(userId, zipcode, score,
+				tariffName);
 
-	static List<LeaderboardEntry> tempLeaderboard = Arrays.asList(
-			new LeaderboardEntry("1", "12345", 1, "super awesome"),
-			new LeaderboardEntry("2", "98765", 1000, "not so awesome"));
+		return leaderboardService.getNeighbors(entry);
+	}
+
 }
