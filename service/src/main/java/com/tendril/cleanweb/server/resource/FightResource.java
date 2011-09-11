a872f7a9f7c85a2559517d16191bd7a2600f88cb
ghengeli <greg.hengeli+github@gmail.com>
2011-09-10 22:25:36 -0700

    resolving my conflicts

--- service/src/main/java/com/tendril/cleanweb/server/resource/FightResource.java
+++ service/src/main/java/com/tendril/cleanweb/server/resource/FightResource.java
@@ -1,12 +1,7 @@
 package com.tendril.cleanweb.server.resource;
 
-<<<<<<< HEAD
-import java.util.Arrays;
-import java.util.List;
-=======
 import com.tendril.cleanweb.domain.Fight;
 import com.tendril.cleanweb.service.FightService;
->>>>>>> why does this thing refuse to commit everything
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -14,81 +9,18 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-import com.tendril.cleanweb.client.GenabilityClient;
-import com.tendril.cleanweb.client.TendrilClient;
-import com.tendril.cleanweb.domain.Fight;
-import com.tendril.cleanweb.domain.genability.TariffInput;
-import com.tendril.cleanweb.domain.tendril.ConsumptionComponent;
-import com.tendril.cleanweb.domain.tendril.CostAndConsumption;
-import com.tendril.cleanweb.server.ConsumptionConverter;
-
 @Path("/fight")
 public class FightResource {
 
-	@Inject
-<<<<<<< HEAD
-	GenabilityClient genabilityClient;
-
-	@Inject
-	TendrilClient tendrilClient;
+    @Inject
+    FightService fightService;
 
 	@GET
 	public Fight fight(@HeaderParam("custom-auth") String auth,
 			@QueryParam("myTariffId") Long myTariffId,
 			@QueryParam("comparisonTariffId") Long comparisonTariffId) {
-		ConsumptionComponent consumptionComponent1 = new ConsumptionComponent();
-		consumptionComponent1.setFromDate("2011-09-01T00:00:00.0-0700");
-		consumptionComponent1.setToDate("2011-10-01T00:00:00.0-0700");
-		consumptionComponent1.setConsumption(100.0d);
 
-		ConsumptionComponent consumptionComponent2 = new ConsumptionComponent();
-		consumptionComponent2.setFromDate("2011-10-01T00:00:00.0-0700");
-		consumptionComponent2.setToDate("2011-11-01T00:00:00.0-0700");
-		consumptionComponent2.setConsumption(120.0d);
-
-		CostAndConsumption costAndConsumption = new CostAndConsumption();
-		costAndConsumption.setComponent(Arrays
-				.asList(new ConsumptionComponent[] { consumptionComponent1,
-						consumptionComponent2 }));
-
-		// CostAndConsumption costAndConsumption =
-		// tendrilClient.getConsumption(auth);
-
-		List<TariffInput> tariffInputs = ConsumptionConverter
-				.convert(costAndConsumption);
-
-		Fight fight = new Fight();
-		double localScore = costAndConsumption.getConsumption()
-				/ genabilityClient.getCost(myTariffId, tariffInputs);
-		fight.setLocalScore((int) localScore);
-		double comparisonScore = genabilityClient.getCost(comparisonTariffId,
-				tariffInputs);
-		fight.setComparisonScore((int) comparisonScore);
-
-		return fight;
+        return fightService.createFight(myTariffId, comparisonTariffId);
 	}
-=======
-    FightService fightService;
-
-	@GET
-	public Fight fight(@HeaderParam("custom-auth") String auth,
-			@QueryParam("tariffId") Long tariffId) {
-//		TendrilLocation tendrilLocation = new TendrilLocation();
-//		tendrilLocation.setPostalCode("80301");
-        // TendrilLocation tendrilLocation = tendrilClient.getLocation(auth);
-
-//		CostAndConsumption costAndConsumption = new CostAndConsumption();
-        // CostAndConsumption costAndConsumption =
-        // tendrilClient.getConsumption(auth);
-
-//		Fight fight = new Fight();
-        // fight.setLocalScore(genabilityClient.getCost(
-        // tendrilLocation.getPostalCode(), costAndConsumption));
-        // fight.setComparisonScore(genabilityClient.getCost(zipCode,
-        // costAndConsumption));
-
-        return fightService.createFight();
-    }
->>>>>>> why does this thing refuse to commit everything
 
 }
