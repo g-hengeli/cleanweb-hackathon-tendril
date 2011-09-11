97f201e2557359bbb2b5dc06640403f4407f2b89
Jacob Kaufman-Osborn <jacobko@gmail.com>
2011-09-11 12:13:41 -0700

    Added derby and leaderboard service.

--- service/src/main/java/com/tendril/cleanweb/server/resource/LeaderboardResource.java
+++ service/src/main/java/com/tendril/cleanweb/server/resource/LeaderboardResource.java
@@ -1,42 +1,54 @@
 package com.tendril.cleanweb.server.resource;
 
-import com.tendril.cleanweb.domain.LeaderboardEntry;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Logger;
 
-/**
- */
+import com.tendril.cleanweb.domain.LeaderboardEntry;
+import com.tendril.cleanweb.service.LeaderboardService;
+
 @Path("/leaderboard")
 @Produces(MediaType.APPLICATION_JSON)
 public class LeaderboardResource {
-    static Logger log = Logger.getLogger(LeaderboardResource.class.getName());
+	static Logger log = Logger.getLogger(LeaderboardResource.class.getName());
 
-//    @POST
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public void createLeaderboardEntry(LeaderboardEntry entry) throws JSONException {
-//        log.info(entry.getZipcode() + " " + entry.getTariffName() + " " + entry.getScore());
-////        log.info(entry);
-//    }
+	@Inject
+	private LeaderboardService leaderboardService;
 
-    @POST
-    public void createLeaderboardEntry(@QueryParam("userId") String userId, @QueryParam("zipcode") String zipcode, @QueryParam("tariffName") String tariffName, @QueryParam("score") int score) {
-        LeaderboardEntry entry = new LeaderboardEntry(userId, zipcode, score, tariffName);
-        log.info(entry.toString());
-    }
+	// @POST
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// public void createLeaderboardEntry(LeaderboardEntry entry) throws
+	// JSONException {
+	// log.info(entry.getZipcode() + " " + entry.getTariffName() + " " +
+	// entry.getScore());
+	// // log.info(entry);
+	// }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<LeaderboardEntry> getLeaderboard() {
-        return tempLeaderboard;
-    }
+	@POST
+	public void createLeaderboardEntry(@QueryParam("userId") String userId,
+			@QueryParam("zipcode") String zipcode,
+			@QueryParam("tariffName") String tariffName,
+			@QueryParam("score") int score) {
+		LeaderboardEntry entry = new LeaderboardEntry(userId, zipcode, score,
+				tariffName);
+		log.info(entry.toString());
+	}
 
-    static List<LeaderboardEntry> tempLeaderboard = Arrays.asList(new LeaderboardEntry("1", "12345", 1, "super awesome"), new LeaderboardEntry("2", "98765", 1000, "not so awesome"));
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<LeaderboardEntry> getLeaderboard() {
+		return tempLeaderboard;
+	}
+
+	static List<LeaderboardEntry> tempLeaderboard = Arrays.asList(
+			new LeaderboardEntry("1", "12345", 1, "super awesome"),
+			new LeaderboardEntry("2", "98765", 1000, "not so awesome"));
 }
---
+++ service/src/main/java/com/tendril/cleanweb/service/LeaderboardService.java
@@ -0,0 +1,158 @@
+package com.tendril.cleanweb.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.tendril.cleanweb.domain.LeaderboardEntry;
+
+public class LeaderboardService {
+
+	private String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+
+	public LeaderboardService() {
+		try {
+			Class.forName(driver).newInstance();
+			System.out.println("Loaded the appropriate driver");
+		} catch (ClassNotFoundException cnfe) {
+			System.err.println("\nUnable to load the JDBC driver " + driver);
+			System.err.println("Please check your CLASSPATH.");
+			cnfe.printStackTrace(System.err);
+		} catch (InstantiationException ie) {
+			System.err.println("\nUnable to instantiate the JDBC driver "
+					+ driver);
+			ie.printStackTrace(System.err);
+		} catch (IllegalAccessException iae) {
+			System.err.println("\nNot allowed to access the JDBC driver "
+					+ driver);
+			iae.printStackTrace(System.err);
+		}
+
+		Connection conn = null;
+		Statement s = null;
+		try {
+			conn = DriverManager
+					.getConnection("jdbc:derby:energyFightDB;create=true");
+			s = conn.createStatement();
+
+			try {
+				s.executeQuery("SELECT COUNT(*) FROM leaderboard");
+			} catch (SQLException sqlException) {
+				s.execute("CREATE TABLE leaderboard(userId VARCHAR(100), zipCode VARCHAR(10), score INT, tariffName VARCHAR(100))");
+			}
+		} catch (Exception e) {
+		} finally {
+			try {
+				if (s != null) {
+					s.close();
+					s = null;
+				}
+			} catch (SQLException sqle) {
+				printSQLException(sqle);
+			}
+
+			try {
+				if (conn != null) {
+					conn.close();
+					conn = null;
+				}
+			} catch (SQLException sqle) {
+				printSQLException(sqle);
+			}
+		}
+	}
+
+	public void persist(LeaderboardEntry leaderboardEntry) {
+		Connection connection = null;
+		ArrayList<Statement> statements = new ArrayList<Statement>();
+		PreparedStatement preparedStatement = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = DriverManager
+					.getConnection("jdbc:derby:energyFightDB;create=true");
+
+			statement = connection.createStatement();
+			statements.add(statement);
+
+			preparedStatement = connection
+					.prepareStatement("SELECT * FROM leaderboard WHERE userId = ?");
+			statements.add(preparedStatement);
+			preparedStatement.setString(1, leaderboardEntry.getUserId());
+
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				preparedStatement = connection
+						.prepareStatement("UPDATE leaderboard SET score = ?, tariffName = ? WHERE userId = ?");
+				statements.add(preparedStatement);
+
+				preparedStatement.setInt(1, leaderboardEntry.getScore());
+				preparedStatement
+						.setString(2, leaderboardEntry.getTariffName());
+				preparedStatement.setString(3, leaderboardEntry.getUserId());
+			} else {
+				preparedStatement = connection
+						.prepareStatement("INSERT INTO leaderboard VALUES (?, ?, ?, ?)");
+				statements.add(preparedStatement);
+
+				preparedStatement.setString(1, leaderboardEntry.getUserId());
+				preparedStatement.setString(2, leaderboardEntry.getZipcode());
+				preparedStatement.setInt(3, leaderboardEntry.getScore());
+				preparedStatement
+						.setString(4, leaderboardEntry.getTariffName());
+			}
+			preparedStatement.executeUpdate();
+		} catch (SQLException sqle) {
+			printSQLException(sqle);
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+					resultSet = null;
+				}
+			} catch (SQLException sqle) {
+				printSQLException(sqle);
+			}
+
+			while (!statements.isEmpty()) {
+				Statement st = (Statement) statements.remove(0);
+				try {
+					if (st != null) {
+						st.close();
+						st = null;
+					}
+				} catch (SQLException sqle) {
+					printSQLException(sqle);
+				}
+			}
+
+			try {
+				if (connection != null) {
+					connection.close();
+					connection = null;
+				}
+			} catch (SQLException sqle) {
+				printSQLException(sqle);
+			}
+		}
+	}
+
+	public static void printSQLException(SQLException e) {
+		// Unwraps the entire exception chain to unveil the real cause of the
+		// Exception.
+		while (e != null) {
+			System.err.println("\n----- SQLException -----");
+			System.err.println("  SQL State:  " + e.getSQLState());
+			System.err.println("  Error Code: " + e.getErrorCode());
+			System.err.println("  Message:    " + e.getMessage());
+			// for stack traces, refer to derby.log or uncomment this:
+			// e.printStackTrace(System.err);
+			e = e.getNextException();
+		}
+	}
+
+}
