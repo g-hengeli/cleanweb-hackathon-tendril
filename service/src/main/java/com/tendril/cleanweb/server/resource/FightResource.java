f991c2862a70c5c865af806ee8d64054b5a131d7
Jacob Kaufman-Osborn <jacobko@gmail.com>
2011-09-10 22:53:43 -0700

    Changed fight resource to use real data.

--- service/src/main/java/com/tendril/cleanweb/server/resource/FightResource.java
+++ service/src/main/java/com/tendril/cleanweb/server/resource/FightResource.java
@@ -1,26 +1,28 @@
 package com.tendril.cleanweb.server.resource;
 
-import com.tendril.cleanweb.domain.Fight;
-import com.tendril.cleanweb.service.FightService;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
+import com.tendril.cleanweb.domain.Fight;
+import com.tendril.cleanweb.service.FightService;
+
 @Path("/fight")
 public class FightResource {
 
-    @Inject
-    FightService fightService;
+	@Inject
+	FightService fightService;
 
 	@GET
 	public Fight fight(@HeaderParam("custom-auth") String auth,
 			@QueryParam("myTariffId") Long myTariffId,
 			@QueryParam("comparisonTariffId") Long comparisonTariffId) {
+		String[] credentials = auth.split(":");
 
-        return fightService.createFight(myTariffId, comparisonTariffId);
+		return fightService.createFight(credentials[0], credentials[1],
+				myTariffId, comparisonTariffId);
 	}
 
 }
